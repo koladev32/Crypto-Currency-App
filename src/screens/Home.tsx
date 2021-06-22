46ccbe7f0a3edc0b104552b41d5ad42652212629
koladev <onaelmangabo@gmail.com>
2021-06-22 13:46:09 +0100

    feat: enhance ui

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Text, View } from "../components/Themed";
 import Layout from "../components/Layout";
@@ -33,9 +33,11 @@ export default function Home() {
           style={{
             backgroundColor: "green",
             height: 180,
-            width: 320,
+            width: 300,
             marginLeft: 16,
             borderRadius: 8,
+            justifyContent: "space-between",
+            padding: "4%",
           }}
           colors={["#b174ff", "#a095fd", "#8eb8fb"]}
           start={{ x: 0.2, y: 1 }}
@@ -43,21 +45,172 @@ export default function Home() {
         >
           <View
             style={{
-              width: "92%",
+              width: "100%",
               backgroundColor: "transparent",
               flexDirection: "row",
               justifyContent: "space-between",
+              alignSelf: "center",
             }}
           >
             <View style={{ backgroundColor: "transparent" }}>
-              <Text>$ 19,329.77</Text>
-              <Text>Your balance is equivalent</Text>
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 32,
+                  fontWeight: "500",
+                }}
+              >
+                $ 19,329.77
+              </Text>
+              <Text style={{ color: "#c3c0fd", marginTop: "2%" }}>
+                Your balance is equivalent
+              </Text>
             </View>
             <View style={{ backgroundColor: "transparent" }}>
-              <Icon name="bitcoin" color="white" type="font-awesome-5" />
+              <Icon name="bitcoin" style={{
+                  transform: [{ rotate: '-12deg'}]
+              }} color="white" type="font-awesome-5" />
             </View>
           </View>
+          <View
+            style={{
+              backgroundColor: "transparent",
+              width: "50%",
+              height: "20%",
+              flexDirection: "row",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                height: 32,
+                backgroundColor: "#ba82ff",
+                width: "50%",
+                borderRadius: 4,
+                justifyContent: "center",
+                marginHorizontal: "2%",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  color: "white",
+                }}
+              >
+                Deposit
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                height: 32,
+                backgroundColor: "#ba82ff",
+                width: "60%",
+                borderRadius: 4,
+                justifyContent: "center",
+                marginHorizontal: "2%",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  color: "white",
+                }}
+              >
+                Withdraw
+              </Text>
+            </TouchableOpacity>
+          </View>
         </LinearGradient>
+          <LinearGradient
+              style={{
+                  backgroundColor: "green",
+                  height: 180,
+                  width: 300,
+                  marginLeft: 16,
+                  borderRadius: 8,
+                  justifyContent: "space-between",
+                  padding: "4%",
+              }}
+              colors={["#e25482", "#e25482", "#8eb8fb"]}
+              start={{ x: 0.2, y: 1 }}
+              end={{ x: 0.7, y: 0.2 }}
+          >
+              <View
+                  style={{
+                      width: "100%",
+                      backgroundColor: "transparent",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignSelf: "center",
+                  }}
+              >
+                  <View style={{ backgroundColor: "transparent" }}>
+                      <Text
+                          style={{
+                              color: "white",
+                              fontSize: 32,
+                              fontWeight: "500",
+                          }}
+                      >
+                          $ 19,329.77
+                      </Text>
+                      <Text style={{ color: "#c3c0fd", marginTop: "2%" }}>
+                          Your balance is equivalent
+                      </Text>
+                  </View>
+                  <View style={{ backgroundColor: "transparent" }}>
+                      <Icon name="bitcoin" style={{
+                          transform: [{ rotate: '-12deg'}]
+                      }} color="white" type="font-awesome-5" />
+                  </View>
+              </View>
+              <View
+                  style={{
+                      backgroundColor: "transparent",
+                      width: "50%",
+                      height: "20%",
+                      flexDirection: "row",
+                  }}
+              >
+                  <TouchableOpacity
+                      style={{
+                          height: "100%",
+                          backgroundColor: "#e5648e",
+                          width: "50%",
+                          borderRadius: 4,
+                          justifyContent: "center",
+                          marginHorizontal: "2%",
+                      }}
+                  >
+                      <Text
+                          style={{
+                              textAlign: "center",
+                              color: "white",
+                          }}
+                      >
+                          Deposit
+                      </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                      style={{
+                          height: "100%",
+                          backgroundColor: "#e5648e",
+                          width: "50%",
+                          borderRadius: 4,
+                          justifyContent: "center",
+                          marginHorizontal: "2%",
+                      }}
+                  >
+                      <Text
+                          style={{
+                              textAlign: "center",
+                              color: "white",
+                          }}
+                      >
+                          Withdraw
+                      </Text>
+                  </TouchableOpacity>
+              </View>
+          </LinearGradient>
       </ScrollView>
 
       <View style={{ flex: 2 }}></View>
